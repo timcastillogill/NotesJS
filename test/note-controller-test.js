ec22a0850b3a8cf9da3bda351450b734dea80254
Tim Castillo-Gill <timgillviola@gmail.com>
2020-11-13 14:46:44 +0000

    Updated folder structure and added mocks

---
+++ test/note-controller-test.js
@@ -0,0 +1,27 @@
+(function() {
+
+  class DoubleNoteController {
+    constructor() {
+      return true;
+    };
+
+    doubleGetHTML() {
+      return "<ul><li><div>Favourite food: pesto</div></li></ul>";
+    }
+  }
+
+function itMakesANewNoteController() {
+    let myNoteController = new NoteController();
+    // let myDoubleNoteList = new DoubleNotelist();
+    assert.isTrue(myNoteController instanceof NoteController);
+    }; itMakesANewNoteController();
+
+    function getHTMLfunctionOutputsFavouriteFood() {
+      let myNoteController = new DoubleNoteController();
+      my
+
+
+    }
+
+
+  })(this);
