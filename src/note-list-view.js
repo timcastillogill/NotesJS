a10bb2de6bd9d37b70156e022826b5f4ab91d697
Tim Castillo-Gill <timgillviola@gmail.com>
2020-11-13 16:54:00 +0000

    Added Note Squasher to stop notes being over 20 characters

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -3,11 +3,20 @@ class NoteListView {
         this.noteList = noteList;
     }
 
+    _noteSquasher(text) {
+      if(text.length > 20) {
+        return text.slice(0, 20);
+      } else {
+        return text;
+      }
+    };
+
     returnHTMLString() {
       let opening = '<ul>'
       let closing = '</ul>'
-      let a = this.noteList.returnNoteList().map(text => "<li><div>" + text.returnText() + "</div></li>").join()
-    
+
+      let a = this.noteList.returnNoteList().map(text => "<li><div>" + this._noteSquasher(text.returnText()) + "</div></li>").join()
+      
       return opening + a + closing
       // return '<ul><li><div>' + test + '</ul></li></div>'
 
--- test/note-list-view-test.js
+++ test/note-list-view-test.js
@@ -10,4 +10,14 @@ function itReturnsHTMLString() {
 
   assert.isTrue(noteListView.returnHTMLString() === '<ul><li><div>test</div></li></ul>')
 }; itReturnsHTMLString();
+
+function onlyShowTheFirstTwentyCharacters() {
+  let noteList = new NoteList();
+  noteList.pushToNotesArray('Cameron has had a superb week, he loves pairing with Tim');
+  let noteListView = new NoteListView(noteList);
+  console.log(noteListView.returnHTMLString());
+  assert.isTrue(noteListView.returnHTMLString() === '<ul><li><div>Cameron has had a su</div></li></ul>');
+};onlyShowTheFirstTwentyCharacters()
+
+
 })(this);
